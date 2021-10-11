00e05667cfa5d31515f782568ddd7e5d61d0e0e3
Mehmet Gökay Yıldız <yildizgokay2@gmail.com>
2021-10-12 00:11:00 +0300

    falan

--- application.py
+++ application.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, session,  Response
-from flask import jsonify
-import requests
+from flask import Flask, render_template
+
 
 application = Flask(__name__)
 
-@application.route("/")
-def hello_world():
-    return "Hello World"
 
-@application.route("/onur")
-def onur():
+
+@application.route("/")
+def odur():
     return render_template("deneme.html")
 
 
 
 if __name__=='__main__':
-    application.run(debug=True)
+    application.run()
